8dea320464319495cbd85a2b9c44725ce91e5eb3
Sinan Cepel <cepel.sinan@gmail.com>
2015-07-05 19:25:23 -0700

    Converted blocks to a DAG

--- Blocks.py
+++ Blocks.py
@@ -2,17 +2,6 @@ from types import *
 
 TYPES = {'INT': int, 'STR': str, 'BOOL': bool, 'LIST': list, 'SET': set, 'DICT': dict, 'ARG': 'argument', 'NONE': None}
 
-# Not working on functions yet
-def collapse(source):
-    output, outputType = source.evaluate()
-    if output is None:
-        collapsed = FunctionBlock(outputType=outputType)
-    else:
-        collapsed = InputBlock(argType=outputType)
-        collapsed.add(output)
-    return collapsed
-
-
 class Block(object):
     _func = lambda: None
     _inputTypes = {}
@@ -21,78 +10,81 @@ class Block(object):
     def __init__(self, name = None, *args, **kwargs):
         self._name = name
         self._inputs = {}
-        self._outputBlock = None
+        self._inputBlocks = {}
         self._value = None
 
     def __str__(self):
-        return "{name}: {value}".format(name=self._name if not self._name is None 
+        return "{name}: {value}".format(name=self._name if not self._name is None
                                         else self._outputType, value=self.getValue())
 
-    def add(self, block, which):
+    def add(self, block, which = None):
+        if which is None:
+            which = len(self._inputBlocks)
         if not which in self._inputTypes.keys():
             raise Exception('Tried to add argument to invalid slot')
         if not self._inputTypes[which] is block.getOutputType() and not block.getOutputType() is 'ARG':
             raise Exception("Incorrect type: These blocks do not have matching types")
-        if which in self._inputs.keys() and not self._inputs[which] is None:
+        if which in self._inputBlocks.keys() and not self._inputBlocks[which] is None:
             raise Exception('This slot if already filled. Remove the block there first')
-        if not block._outputBlock is None:
-            raise Exception('The block you are trying to add already has an output.')
-        self._inputs[which] = block
+        self._inputBlocks[which] = block
         block._outputBlock = self
 
     def remove(self, which):
         if which in self._inputTypes.keys():
-            if which in self._inputs.keys() and not self._inputs[which] is None:
-                self._inputs[which]._outputBlock = None
-                self._inputs[which] = None
+            if which in self._inputBlocks.keys() and not self._inputBlocks[which] is None:
+                self._inputBlocks[which] = None
                 self.clearValue()
             else:
                 raise Exception('There is no block to remove from this slot')
         else:
             raise Exception('Tried to remove argument from invalid slot')
-    
+
     def clearValue(self):
         self._value = None
-        if (not self._outputBlock is None):
-            self._outputBlock.clearValue()
 
     def getValue(self):
         return self._value
+
+    def getType(self):
+        types = []
+        for k in self._inputTypes:
+            types.add(self._inputTypes[k])
+        return types
 
     def getName(self):
         return self._name
 
     def setName(self, name):
-        self._name = name     
+        self._name = name
 
     def getOutputType(self):
         return self._outputType
 
     def evaluate(self):
-        if None in self._inputs.values() or len(self._inputs) != len(self._inputTypes):
+        # Don't repeat work if we already have a value
+        if self._value is not None:
+            return self._value, self._outputType
+        if len(self._inputBlocks) != len(self._inputTypes):
             raise Exception("Not enough arguments provided")
-        inputValues = map(lambda block: (block, block.getValue()), self._inputs.values())
-        for block, blockValue in inputValues:
-            if block.getOutputType() is 'ARG':
-                return (None, self._outputType)
-            if blockValue is None:
-                raise Exception("You must evaluate this block's parents first.")
-        self._value = self._func(*map(lambda (block, val): val, inputValues))
-        if (not self._outputBlock is None):
-            return self._outputBlock.evaluate()
-        else:
-            return (self._value, self._outputType)
+        for k in self._inputBlocks:
+            val = self._inputBlocks[k]
+            v, t = val.evaluate()
+            if t != self._inputTypes[k]:
+                raise Exception("Argument type mismatch")
+            self._inputs[k] = v
+        self._value = self._func(*(self._inputs.values()))
+        return self._value, self._outputType
 
 class InputBlock(Block):
     _type = 'ARG'
     _func = lambda self: self._value
-    
+
     def __init__(self, *args, **kwargs):
         self._outputType = 'ARG'
         if 'argType' in kwargs.keys():
             self._type = kwargs['argType']
         super(InputBlock, self).__init__(*args, **kwargs)
-    
+
     def add(self, value):
         if not type(value) is TYPES[self._type]:
             raise Exception("Incorrect type: {value} does not have type {typeKey}"
@@ -113,13 +105,13 @@ class ListBlock(InputBlock):
     _type = 'LIST'
 
 class FunctionBlock(Block):
-    
+
     def __init__(self, *args, **kwargs):
         if 'outputType' in kwargs.keys():
             self.outputType = kwargs['outputType']
         super(FunctionBlock, self).__init__(*args, **kwargs)
         self._inputs = {i: None for i in xrange(len(self._inputTypes))} # This is the input block
-    
+
 class StringTransformBlock(FunctionBlock):
     _inputTypes = {0: 'STR'}
     _outputType = 'STR'
@@ -153,7 +145,7 @@ class MultBlock(BinaryMathBlock):
     _func = staticmethod(lambda value1, value2: value1 * value2)
 
 class DivBlock(BinaryMathBlock):
-    _func = staticmethod(lambda value1, value2: value1 / value2)    
+    _func = staticmethod(lambda value1, value2: value1 / value2)
 
 class MapBlock(FunctionBlock):
     _inputTypes = {0: 'ARG', 1: 'LIST'}
