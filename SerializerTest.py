f64bdc98a9f183a1f3eccd26041138b8d4fa2a80
Nathaniel Biggs <biggs0125@gmail.com>
2015-07-08 20:08:31 -0400

    Serialize Tests fixed and added a test

--- SerializerTest.py
+++ SerializerTest.py
@@ -7,9 +7,13 @@ print "###########Serialize Tests###############"
 block1 = Test.e1
 print block1.evaluate()
 block1.foldFunc()
-block1._func = Closure(block1._func)
-block1._func.fold()
 block1str = cPickle.dumps(block1)
 block2 = cPickle.loads(block1str)
 print block2.evaluate()
+block3 = Test.f1
+print block3.evaluate()
+block3.foldFunc()
+block3str = cPickle.dumps(block3)
+block4 = cPickle.loads(block3str)
+print block4.evaluate()
 print "#########################################"
